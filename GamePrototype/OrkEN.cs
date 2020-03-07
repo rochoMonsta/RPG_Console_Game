932b060929b6b2c5000f95bb30cfdf16aa74061c
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-03-07 23:05:27 +0200

    git add

---
+++ GamePrototype/OrkEN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GamePrototype
+{
+    public class OrkEN : Enemy
+    {
+        public OrkEN() { }
+        public OrkEN(Character character)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            Name = "Orc robber";
+            Atack_Power = 25 + (character.CharacterLevel.CHARACTER_LEVEL_GS * 20);
+            Level = character.CharacterLevel.CHARACTER_LEVEL_GS + random.Next(0, 4);
+            Health = 50 + character.health.Health_GS;
+            Anymation = @"\C:OrkRobber.animanion";
+        }
+    }
+}
